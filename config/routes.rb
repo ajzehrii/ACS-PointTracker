5922fe16d22e108d58a9984ebb3c79fd2208b1d3
unknown <ajzehri@gmail.com>
2022-09-18 16:35:50 -0500

    basic website layout outline

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get"home", to: "pages#home"
+  get"about", to: "pages#about"
+  get"officer-team", to: "pages#officer-team"
+  get"general-meetings", to: "pages#general-meetings"
+  get"admin", to: "pages#admin"
+
   resources :members
-  root 'members#index'
+  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
